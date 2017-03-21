0b1635eb5187b598f29e47f9e54fa58a74fcb20a
Rishi Thakkar <rishirt.us@gmail.com>
2017-03-21 18:52:17 -0500

    Only STI and LDI left. Need to implement stalling for this.

--- testcode/cp2/test-shf.asm
+++ testcode/cp2/test-shf.asm
@@ -19,44 +19,194 @@ CODE:
 	;; in ModelSim match the LC3bSimulator.
 	;;
 
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 	LEA R0, DATA
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 
 	;; Load values into registers
 	LDR R1, R0, RSHFL_TVAL
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 	LDR R2, R0, RSHFL_TVAL
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 	LDR R3, R0, RSHFL_TVAL
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 	LDR R4, R0, RSHFL_TVAL
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 	LDR R5, R0, RSHFL_TVAL
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 	LDR R6, R0, RSHFL_TVAL
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 	LDR R7, R0, RSHFL_TVAL
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 
 	;; Testing RSHFL
 	RSHFL R1, R1, 15		;; R1 <- 0x0001
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 	RSHFL R2, R2, 14		;; R2 <- 0x0003
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 	RSHFL R3, R3, 13		;; R3 <- 0x0007
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 	RSHFL R4, R4, 12		;; R4 <- 0x000F
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 	RSHFL R5, R5, 11		;; R5 <- 0x001F
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 	RSHFL R6, R6, 10		;; R6 <- 0x003F
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 	RSHFL R7, R7, 9			;; R7 <- 0x007F
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 
 	;; Testing RSHFA
 	LDR R4, R0, RSHFA_TVAL
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 	RSHFA R3, R4, 8			;; R3 <-0xFF80
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 	RSHFA R2, R3, 7			;; R2 <-0xFFFF
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 	LDR R2, R0, RSHFA_TVAL
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 	RSHFA R1, R2, 6			;; R1 <-0xFE00
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 
 	;; Testing LSHF
 	LDR R1, R0, LSHF_TVAL
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 	LSHF R1, R1, 0			;; R1 <- 0x0001
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 	LSHF R2, R1, 1			;; R2 <- 0x0002
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 	LSHF R3, R2, 2			;; R3 <- 0x0008
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 	LSHF R4, R3, 3			;; R4 <- 0x0040
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 	LSHF R5, R4, 4			;; R5 <- 0x0400
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 	LSHF R6, R5, 5			;; R6 <- 0x8000
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 
 HALT_PROG:
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 	BRnzp HALT_PROG
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 
 SEGMENT
 DATA:
--- testcode/cp2/test-trap.asm
+++ testcode/cp2/test-trap.asm
@@ -29,19 +29,82 @@ CODE:
 	;;
 
 	;; Testing Trap
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 	LEA R0, TRAP_ARGS
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 	LDB R2, R0, 0			;; load byte: LDB_TVAL + 0
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 	LDB R3, R0, 1			;; load byte: LDB_TVAL + 1
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 	TRAP PTR_TRAP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	ADD R2, R4, 0
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	TRAP PTR_TRAP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 
 HALT_PROG:
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 	BRnzp HALT_PROG
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 
 ;; input: R2, R3
 ;; output: R4
 TRAP_ADD_FUNC:
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 	ADD R4, R3, R2
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 	RET
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 
 SEGMENT
 DATA:
