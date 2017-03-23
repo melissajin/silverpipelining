62f2106e6ad7b0d453d179d382a020801545a3fc
Rishi Thakkar <rishirt.us@gmail.com>
2017-03-23 17:16:01 -0500

    Started hazard detection

---
+++ mp0/hazard_detection.sv
@@ -0,0 +1,15 @@
+
+module hazard_detection
+(
+    /* inputs */
+    input logic i_mem_resp, d_mem_resp, d_mem_read,
+    input lc3b_opcode op_MEM, op_WB,
+
+    /* outputs */
+    
+
+);
+
+
+
+endmodule // hazard_detection
