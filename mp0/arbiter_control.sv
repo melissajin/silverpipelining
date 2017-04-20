094e952e31e6ace7c4314de6d985df13f4272413
Rishi Thakkar <rishirt.us@gmail.com>
2017-04-20 12:44:54 -0500

    Got rid of extra buffer state in arbiter. Everything still works.

--- mp0/arbiter_control.sv
+++ mp0/arbiter_control.sv
@@ -13,7 +13,7 @@
 
 	// Inputs driving control decisions
 	input i_cache_read_in, d_cache_read_in,
-	input i_cache_write_in, d_cache_write_in, // i_cache_write_in will always be 0
+	input i_cache_write_in, d_cache_write_in,
 	input l2_resp_in,
 
 	// Output to datapath
@@ -22,16 +22,13 @@
 
  /* List of states */
  enum int unsigned {
-	 idle, d_cache, i_cache, buffer
+	 idle, d_cache, i_cache
  } state, next_state;
 
 
 always_comb
 begin : state_actions
-	/* Default output assignments */
 	cache_arbiter_sel = 1'b0;
-
-	/* Actions for each state */
     if(state == d_cache)
         cache_arbiter_sel = 1'b1;
 end
@@ -40,19 +37,16 @@ always_comb
 begin : next_state_logic
 	next_state = state;
 
-	/* Next state information and conditions (if any) for transitioning between states */
 	case(state)
 		idle: begin
     		if(d_cache_read_in | d_cache_write_in)
 				next_state = d_cache;
     		else if(i_cache_read_in | i_cache_write_in)
 				next_state = i_cache;
-    		else
-				next_state = idle;
 		end
 		d_cache: begin
 			if(l2_resp_in)
-					next_state = buffer;
+					next_state = idle;
 			else begin
                 if(d_cache_read_in | d_cache_write_in)
                     next_state = d_cache;
@@ -62,7 +56,7 @@ begin : next_state_logic
 		end
 		i_cache: begin
 			if(l2_resp_in)
-                next_state = buffer;
+                next_state = idle;
 			else begin
                 if(i_cache_read_in | i_cache_write_in)
                     next_state = i_cache;
@@ -70,21 +64,12 @@ begin : next_state_logic
                     next_state = idle;
 			end
 		end
-        buffer: begin
-            if(d_cache_read_in | d_cache_write_in)
-                next_state = d_cache;
-            else if(i_cache_read_in | i_cache_write_in)
-                next_state = i_cache;
-            else
-                next_state = idle;
-        end
 		default: begin ; end // Nothing
 	endcase
 end
 
 always_ff @(posedge clk)
 begin: next_state_assignment
-	/* Assignment of next state on clock edge */
 	state <= next_state;
 end
 
